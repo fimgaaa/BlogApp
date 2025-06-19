cd04417ce4b8d3186aaf0aad46ef9d53cd728616
fimgaaa <Feyzax3333@gmail.com>
2025-06-20 02:23:15 +0300

    main

--- blog_app_son/lib/main.dart
+++ blog_app_son/lib/main.dart
@@ -1,11 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
+import 'firebase_options.dart';
 import 'home_page.dart';
 
-void main() {
+//void main() {
+  //runApp(MyApp());
+//}
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MyApp());
 }
-
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
