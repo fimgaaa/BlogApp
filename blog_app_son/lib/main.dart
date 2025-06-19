7f622f0cc3f6415f22f858f93ac2762c9df5671b
fimgaaa <Feyzax3333@gmail.com>
2025-06-20 02:23:25 +0300

    main

--- blog_app_son/lib/main.dart
+++ blog_app_son/lib/main.dart
@@ -1,28 +1,25 @@
-import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
 
 import 'firebase_options.dart';
 import 'home_page.dart';
 
 //void main() {
-  //runApp(MyApp());
+//runApp(MyApp());
 //}
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(MyApp());
 }
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Blog Uygulaması',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      theme: ThemeData(primarySwatch: Colors.blue),
       home: HomePage(),
     );
   }
