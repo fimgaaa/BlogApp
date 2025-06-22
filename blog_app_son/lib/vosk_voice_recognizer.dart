b3fa9e4c251e8f4b6f91fb8fef164bf67b331e18
fimgaaa <Feyzax3333@gmail.com>
2025-06-22 06:12:31 +0300

    kaldır

--- blog_app_son/lib/vosk_voice_recognizer.dart
+++ blog_app_son/lib/vosk_voice_recognizer.dart
@@ -3,6 +3,7 @@ import 'package:vosk_flutter/vosk_flutter.dart';
 class VoskVoiceRecognizer {
   late SpeechService _speechService;
 
+<<<<<<< HEAD
   Future<void> init() async {
     final modelLoader = ModelLoader();
     final modelPath = await modelLoader.loadFromAssets(
@@ -32,5 +33,31 @@ class VoskVoiceRecognizer {
 
   Future<void> stopListening() async {
     await _speechService.stop();
+=======
+/// Simplified recognizer implementation using the Vosk plugin.
+class VoskVoiceRecognizer implements IVoiceRecognizer {
+  final VoskFlutter _vosk = VoskFlutter();
+
+  @override
+  Future<bool> initialize() async {
+    await _vosk.init();
+    return true;
+  }
+
+  @override
+  void listen({
+    required void Function(String text, bool finalResult) onResult,
+  }) {
+    _vosk.start(
+      onResult: (text, isFinal) {
+        onResult(text, isFinal);
+      },
+    );
+  }
+
+  @override
+  void stop() {
+    _vosk.stop();
+>>>>>>> parent of ddb8620 (vosk fix model indirme)
   }
 }
