882ed4c6cf1995258197521c14a7f26c26e677b3
fimgaaa <Feyzax3333@gmail.com>
2025-06-22 06:14:53 +0300

    kaldır

--- blog_app_son/lib/vosk_voice_recognizer.dart
+++ blog_app_son/lib/vosk_voice_recognizer.dart
@@ -1,5 +1,6 @@
 import 'package:vosk_flutter/vosk_flutter.dart';
 
+<<<<<<< HEAD
 class VoskVoiceRecognizer {
   late SpeechService _speechService;
 
@@ -65,5 +66,35 @@ class VoskVoiceRecognizer implements IVoiceRecognizer {
 >>>>>>> parent of ddb8620 (vosk fix model indirme)
 =======
 >>>>>>> parent of ddb8620 (vosk fix model indirme)
+=======
+import 'voice_recognizer.dart';
+
+class VoskVoiceRecognizer implements IVoiceRecognizer {
+  late final SpeechService _speechService;
+
+  @override
+  Future<bool> initialize() async {
+    final model = await VoskModel.loadAssets(
+      'assets/models/vosk-model-small-tr-0.3',
+    );
+    _speechService = SpeechService(model);
+    return true;
+  }
+
+  @override
+  void listen({
+    required void Function(String text, bool finalResult) onResult,
+  }) {
+    _speechService.start(
+      onResult: (text, isFinal) {
+        onResult(text, isFinal);
+      },
+    );
+  }
+
+  @override
+  void stop() {
+    _speechService.stop();
+>>>>>>> parent of 4ef59d3 (vosk fix)
   }
 }
